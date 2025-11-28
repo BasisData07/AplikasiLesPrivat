533bdd5074cb66f308b1f34e068e45b48ed49956
Byeone001 <halimqolbun@gmail.com>
2025-11-28 09:59:36 +0700

    New Baru

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,10 +9,10 @@ import 'dotenv/config';
 // 2. Gunakan createPool (Lebih stabil daripada createConnection untuk API)
 const db = mysql.createPool({
   // GANTI 'localhost' JADI '127.0.0.1'
-  host: process.env.DB_HOST || '127.0.0.1', 
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'aplikasi_les_mania',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
